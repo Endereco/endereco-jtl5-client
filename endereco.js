eebfd1b1a874975d0ac140e0d7345035d4e89966
André Lötzgen <info@simplecode.de>
2025-07-02 17:53:02 +0200

    Refactor typeahead handling for address fields
    Introduces a TypeaheadManager class to handle the clean removal of typeahead instances on defined input fields.
    
    This change improves the reliability of typeahead removal by observing DOM changes and ensuring that typeahead is destroyed when the input fields are dynamically modified or re-rendered. It also centralizes the logic for managing typeahead instances, making the code more maintainable and testable.
    
    Fixes #9

--- endereco.js
+++ endereco.js
@@ -94,33 +94,74 @@ if (window.EnderecoIntegrator) {
     window.EnderecoIntegrator = EnderecoIntegrator;
 }
 
+window.EnderecoIntegrator.TypeaheadManager = class TypeaheadManager {
+    static SELECTORS = {
+        ZIP_INPUT: 'input[name="plz"]',
+        CITY_INPUT: 'input[name="ort"]',
+        SHIPPING_ZIP: '[name="register[shipping_address][plz]"]',
+        SHIPPING_CITY: '[name="register[shipping_address][ort]"]'
+    };
+
+    constructor() {
+        Object.values(TypeaheadManager.SELECTORS).forEach(selector => {
+            const element = document.querySelector(selector);
+
+            if (element) {
+                this.removeFromElement(element);
+            }
+        });
+    }
+
+    removeFromElement(element) {
+        if (this.isInitialized(element)) {
+            this.destroy(element);
+        } else {
+            this.observeChanges(element);
+        }
+    }
+
+    destroy(element) {
+
+        // check for jQuery and the typeahead plugin
+        if (!$?.fn?.typeahead) {
+            return;
+        }
+
+        $(element).typeahead('destroy');
+
+        // remove remains from JTL's typeahead implementation
+        element.classList.remove('bg-typeahead-fix');
+        element.classList.remove('typeahead');
+
+        const container = element.closest('.typeahead-required');
+
+        if (container) {
+            container.classList.remove('typeahead-required');
+        }
+    }
+
+    observeChanges(element) {
+        const observer = new MutationObserver(() => {
+            if (this.isInitialized(element)) {
+                observer.disconnect();
+                this.destroy(element);
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            attributes: true,
+            subtree: true
+        });
+    }
+
+    isInitialized(element) {
+        return element.classList.contains('tt-input');
+    }
+}
+
 window.EnderecoIntegrator.waitUntilReady().then( function() {
-    var $removeTypeahead = setInterval( function() {
-        if (
-            $ &&
-            $('[name="ort"]').length &&
-            $('[name="ort"]')[0].classList.contains('tt-input')
-        ) {
-            $('.city_input').typeahead('destroy');
-            $('.city_input').removeClass('bg-typeahead-fix');
-            $('[name="ort"]').closest('.typeahead-required').removeClass('typeahead-required');
-
-            clearInterval($removeTypeahead);
-        }
-    }, 100);
-
-    var $removeTypeahead2 = setInterval( function() {
-        if (
-            $ &&
-            $('[name="register[shipping_address][ort]"]').length &&
-            $('[name="register[shipping_address][ort]"]').typeahead
-        ) {
-            $('[name="register[shipping_address][ort]"]').typeahead('destroy');
-            $('[name="register[shipping_address][ort]"]').removeClass('bg-typeahead-fix');
-            $('[name="register[shipping_address][ort]"]').closest('.typeahead-required').removeClass('typeahead-required');
-            clearInterval($removeTypeahead2);
-        }
-    }, 100);
+    new window.EnderecoIntegrator.TypeaheadManager();
 });
 
 var $waitForConfig = setInterval( function() {
